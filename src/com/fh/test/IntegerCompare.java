34fbbc56d4e456d0398ac98a7f53fc87de071009
zoro <swordman_zoro@163.com>
2017-04-24 22:07:48 +0800

    11111

---
+++ src/com/fh/test/IntegerCompare.java
@@ -0,0 +1,12 @@
+package com.fh.test;
+
+public class IntegerCompare {
+	public static void main(String[] args) {
+		Integer i1 = new Integer(5);
+		Integer i2 = new Integer(5);
+		Integer i3 = new Integer(200);
+		Integer i4 = new Integer(200);
+	    System.out.println(i1 == i2);
+	    System.out.println(i3 == i4);
+	}
+}
