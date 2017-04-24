560b6e1cacc3fbce0071091c84283bea787158c0
zoro <swordman_zoro@163.com>
2017-04-24 22:25:08 +0800

    bendi

---
+++ src/com/fh/test/IntegerCompare2.java
@@ -0,0 +1,19 @@
+package com.fh.test;
+
+public class IntegerCompare2 {
+	public static void main(String[] args) {
+		Integer i1 = new Integer(5);
+	    Integer i7 = 5;
+	    Integer i8 = 5;
+	    int i6 = 5;
+	    System.out.println(i6 == i1);
+	    System.out.println(i7 == i8);
+	    
+	    int i = 200;
+	    Integer i9 = 200;
+	    Integer i10 = 200;
+	    System.out.println(i == i9);
+	    System.out.println(i9 == i10);
+	    
+	}
+}
